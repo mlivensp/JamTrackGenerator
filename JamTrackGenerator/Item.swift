5c82e5c993e0603084e227e81799d441cc002548
Michael Livenspargar <mlivensp@gmail.com>
2025-08-18 10:04:35 -0700

    Initial Commit

---
+++ JamTrackGenerator/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  JamTrackGenerator
+//
+//  Created by Michael Livenspargar on 8/18/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ JamTrackGeneratorTests/JamTrackGeneratorTests.swift
@@ -0,0 +1,16 @@
+//
+//  JamTrackGeneratorTests.swift
+//  JamTrackGeneratorTests
+//
+//  Created by Michael Livenspargar on 8/18/25.
+//
+
+import Testing
+
+struct JamTrackGeneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
